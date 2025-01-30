4b8ab2bd3a4f8a0221a50e732eb386258c519016
Pavlo Stovba <stovbapavlo@gmail.com>
2025-01-30 13:37:27 +0200

    Resolved merge conflicts for feature/sorting-and-filtering

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -14,14 +14,14 @@ export function Sort({ value, onChangeSort, order, setOrder }) {
   };
 
   const toggleOrder = () => {
-    setOrder(order === 'desc' ? 'asc' : 'desc'); // Перемикаємо порядок
+    setOrder(order === 'desc' ? 'asc' : 'desc');
   };
 
   return (
     <div className="sort">
       <div className="sort__label">
         <svg
-          onClick={toggleOrder} // Додаємо обробник подій
+          onClick={toggleOrder}
           width="10"
           height="6"
           viewBox="0 0 10 6"
@@ -30,8 +30,7 @@ export function Sort({ value, onChangeSort, order, setOrder }) {
           style={{
             transform: order === 'desc' ? 'rotate(0deg)' : 'rotate(180deg)',
             cursor: 'pointer',
-          }} // Обертаємо стрілку
-        >
+          }}>
           <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
